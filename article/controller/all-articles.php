9c4f64ac522da139b561ff73deecd036febb5f30
brint <brint@blendimc.com>
2014-10-16 13:29:37 -0500

    Full Article List

---
+++ article/controller/all-articles.php
@@ -0,0 +1,39 @@
+<?php if(!defined("CONF_PATH")) { die("No direct script access allowed."); }
+
+// Get a list of the user's Content Entries
+$contentIDs = ContentFeed::getRecentEntryIDs(Content::STATUS_GUEST);
+
+// Prepare the Content Feed
+ContentFeed::prepare();
+
+/****** Page Configurations ******/
+$config['canonical'] = "/all-articles";
+$config['pageTitle'] = 'All Articles - ' . $config['site-name'];
+Metadata::$index = false;
+Metadata::$follow = true;
+
+// Run Global Script
+require(CONF_PATH . "/includes/global.php");
+
+// Display the Header
+require(SYS_PATH . "/controller/includes/metaheader.php");
+require(SYS_PATH . "/controller/includes/header.php");
+
+// Display Side Panel
+require(SYS_PATH . "/controller/includes/side-panel.php");
+
+echo '
+<div id="panel-right"></div>
+<div id="content">' . Alert::display();
+
+// Display the Feed Header
+ContentFeed::displayHeader("All Articles", $config['site-name'], "/");
+
+// Display the Feed
+ContentFeed::displayFeed($contentIDs, true, Me::$id);
+
+echo '
+</div>';
+
+// Display the Footer
+require(SYS_PATH . "/controller/includes/footer.php");
--- sports/includes/global.php
+++ sports/includes/global.php
@@ -17,6 +17,7 @@ if(Me::$loggedIn)
 	<div class="panel-box" style="min-height:10px;">
 		<ul class="panel-notes" style="margin-top:10px;">
 			<li class="nav-note"><a href="/user/' . Me::$vals['handle'] . '">My Articles</a></li>
+			<li class="nav-note"><a href="/all-articles">All Articles</a></li>
 		</ul>
 	</div>';
 	
