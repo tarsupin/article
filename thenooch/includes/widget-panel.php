c39ec89bc74a1bcaa6b29272f9df00c3e3bba306
brint <brint@blendimc.com>
2014-11-10 17:27:15 -0600

    Preparing featured widgets

--- thenooch/includes/widget-panel.php
+++ thenooch/includes/widget-panel.php
@@ -1,8 +1,8 @@
 <?php if(!defined("CONF_PATH")) { die("No direct script access allowed."); }
 
 // Prepare the Featured Widget Data
-$hashtag = "";
-$categories = array("articles", "people");
+$hashtag = "Humor";
+$categories = array("articles", "people", "communities");
 
 // Create a new featured content widget
 $featuredWidget = new FeaturedWidget($hashtag, $categories);
